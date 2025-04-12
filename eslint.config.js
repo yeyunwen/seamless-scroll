62d5697b14127c4a0fc87681c1c8641e28890d26
yeyunwen <1571374338@qq.com>
2025-04-12 16:03:55 +0800

    chore(eslint): 忽略dist文件夹下的eslint校验

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,14 @@
 // @ts-check
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(tseslint.configs.recommended, {
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off",
+export default tseslint.config(
+  tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
   },
-});
+  {
+    ignores: ["**/dist/**"],
+  },
+);
