3923d3d2d0d061abc68f534f0bbb928f9ce3b2aa
yeyunwen <1571374338@qq.com>
2025-04-14 00:06:12 +0800

    feat(cicd): 添加不是docs的CD

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   lang: "zh-CN",
   lastUpdated: true,
   cleanUrls: true,
+  base: "/seamless-scroll/",
 
   head: [["link", { rel: "icon", type: "image/svg+xml", href: "/logo.svg" }]],
 
