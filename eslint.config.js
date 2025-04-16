6429694e56b01d303c463d55396921ea4b19e0b4
yeyunwen <1571374338@qq.com>
2025-04-16 23:49:59 +0800

    chore: 添加react的eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,24 @@
 // @ts-check
 import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
 export default tseslint.config(
-  tseslint.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+  {
+    files: ["**/react/**/*.{ts,tsx}"],
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": "off",
     },
   },
   {
