ed6b1a6e592d7f20807722a675b6f906546eed35
yeyunwen <1571374338@qq.com>
2025-04-26 22:55:22 +0800

    chore: 给vue文件添加eslint，更新vue包tsconfig

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,10 @@
 // @ts-check
 import tseslint from "typescript-eslint";
+import tsParser from "@typescript-eslint/parser";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import vueParser from "vue-eslint-parser";
+import vuePlugin from "eslint-plugin-vue";
 
 export default tseslint.config(
   ...tseslint.configs.recommended,
@@ -19,6 +22,18 @@ export default tseslint.config(
     rules: {
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": "off",
+    },
+  },
+  {
+    files: ["**/vue/**/*.vue"],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+      },
+    },
+    plugins: {
+      vue: vuePlugin,
     },
   },
   {
